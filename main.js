c1ab1a22b2c23c285e48064c9de69adbc352ba25
Jesse Seligman <jesseseligman@gmail.com>
2016-11-03 23:14:46 -0700

    Create simple node server

--- main.js
+++ main.js
@@ -3,3 +3,23 @@
 const express = require('express');
 const port = process.env.PORT || 8000;
 const path = require('path');
+var fs = require("fs");
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/locations', function(req, res) {
+  var locations = fs.readFileSync("./location.json");
+  console.log(JSON.parse(locations));
+});
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+
+module.exports = app;
