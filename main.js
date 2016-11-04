6a44090ab837b2a99bdff96db10440ef180c8129
Jesse Seligman <jesseseligman@gmail.com>
2016-11-04 04:27:15 -0700

    Add distance notification on map tracker

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const port = process.env.PORT || 8000;
 const path = require('path');
-var fs = require("fs");
+const fs = require("fs");
+const geolib = require('geolib');
 
 const bodyParser = require('body-parser');
 
@@ -14,17 +15,19 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/locations', function(req, res) {
-  var locations = fs.readFileSync("./location.json");
-  res.send(JSON.parse(locations).user1);
+  fs.readFile("./location.json", function(err, data) {
+    const locations = JSON.parse(data).user1;
+    const distance = () => geolib.getDistance({latitude: locations[locations.length - 1].lat, longitude: locations[locations.length - 1].lng}, {latitude: locations[locations.length - 3].lat, longitude: locations[locations.length - 3].lng});
+    res.send({ distance: distance(), location: locations[locations.length - 1]});
+  });
 });
+
 
 app.post('/locations', function(req, res) {
   var lat = Number.parseFloat(req.body.latitude);
   var lng = Number.parseFloat(req.body.longitude);
-  console.log(lat);
-  console.log(lng);
-  var coords = { lat: lat, lng: lng };
 
+  var coords = { lat: lat, lng: lng };
   fs.readFile('./location.json', 'utf8', function readFileCallback(err, data) {
     if (err) {
         console.log(err);
@@ -33,10 +36,13 @@ app.post('/locations', function(req, res) {
       var obj = JSON.parse(data); //now it an object
       obj.user1.push(coords); //add some data
       var json = JSON.stringify(obj); //convert it back to json
-      fs.writeFileSync('./location.json', json, 'utf8'); // write it back
-      res.send('success')
+      fs.writeFile('./location.json', json, 'utf8', function() {
+        res.send('success')
+      }); // write it back
+
     }
   });
+
 });
 
 app.listen(port, () => {
