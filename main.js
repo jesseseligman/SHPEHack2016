48075000e73e24c4412b28cd3208e97922367d12
Jesse Seligman <jesseseligman@gmail.com>
2016-11-04 00:32:15 -0700

    Finish post location route

--- main.js
+++ main.js
@@ -15,23 +15,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/locations', function(req, res) {
   var locations = fs.readFileSync("./location.json");
-  console.log(JSON.parse(locations));
+  res.send(JSON.parse(locations));
 });
 
 app.post('/locations', function(req, res) {
   var user = req.body.user;
   var coords = req.body.coords;
+  console.log(coords);
 
-  // fs.readFile('./location.json', 'utf8', function readFileCallback(err, data){
-  //   if (err){
-  //       console.log(err);
-  //   }
-  //   else {
-  //     obj = JSON.parse(data); //now it an object
-  //     obj.table.push({id: 2, square:3}); //add some data
-  //     json = JSON.stringify(obj); //convert it back to json
-  //     fs.writeFileSync('myjsonfile.json', json, 'utf8', callback); // write it back
-  //   });
+  fs.readFile('./location.json', 'utf8', function readFileCallback(err, data) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+      var obj = JSON.parse(data); //now it an object
+      console.log(obj);
+      obj.user1.push(coords); //add some data
+      var json = JSON.stringify(obj); //convert it back to json
+      fs.writeFileSync('./location.json', json, 'utf8'); // write it back
+      res.send('success')
+    }
+  });
 });
 
 app.listen(port, () => {
