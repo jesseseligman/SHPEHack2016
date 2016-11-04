8519bfa58f500ec74d67382144d2b870d57ffd67
Jesse Seligman <jesseseligman@gmail.com>
2016-11-03 23:31:18 -0700

    Start post locations route

--- main.js
+++ main.js
@@ -18,6 +18,22 @@ app.get('/locations', function(req, res) {
   console.log(JSON.parse(locations));
 });
 
+app.post('/locations', function(req, res) {
+  var user = req.body.user;
+  var coords = req.body.coords;
+
+  // fs.readFile('./location.json', 'utf8', function readFileCallback(err, data){
+  //   if (err){
+  //       console.log(err);
+  //   }
+  //   else {
+  //     obj = JSON.parse(data); //now it an object
+  //     obj.table.push({id: 2, square:3}); //add some data
+  //     json = JSON.stringify(obj); //convert it back to json
+  //     fs.writeFileSync('myjsonfile.json', json, 'utf8', callback); // write it back
+  //   });
+});
+
 app.listen(port, () => {
   console.log('Listening on port', port);
 });
